dd8652056d2f0c85b712dfe3d6ec9914d3234bb8
vveyuux <vbussararungsee@gmail.com>
2023-05-20 17:34:59 +0700

    fix ejs view in app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 const port = process.env.PORT || 3000;
